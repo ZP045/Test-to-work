2403380a7757bb81c5a75badb5849e289324a051
Jaine Souza <97619330+ZP045@users.noreply.github.com>
2022-05-25 20:08:59 -0300

    Update Cadastrar.php

--- CadastrodeUsuarios/Cadastrar.php
+++ CadastrodeUsuarios/Cadastrar.php
@@ -1,1 +1,35 @@
+<?php
 
+include_once "conexao.php";
+
+dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+if (empty($dados['nome'])) {
+    $retorna = ['erro' => true, 'msg' => "<div class ='alert alert-danger' role ='alert'>Erro: Necessário preencher o campo nome!</div>"];
+}elseif (empty($dados['email'])) {
+   $retorna = ['erro' => true, 'msg' => "<div class ='alert alert-danger' role ='alert'>Erro: Necessário preencher o campo e-mail!</div>"];
+}
+elseif (empty($dados['cpf'])) {
+   $retorna = ['erro' => true, 'msg' => "<div class ='alert alert-danger' role ='alert'>Erro: Necessário preencher o campo CPF!</div>"];
+}
+elseif (empty($dados['telefone'])) {
+   $retorna = ['erro' => true, 'msg' => "<div class ='alert alert-danger' role ='alert'>Erro: Necessário preencher o campo Telefone!</div>"];
+}
+}
+else {
+  $query_usuario = "INSERT INTO usuarios (nome, email, cpf, telefone) VALUES (:nome, :email, :cpf, :telefone)";
+  $cad_usuario = $cann-> prepare($query_usuario);
+  $cad_usuário->binParam(':nome', $dados['nome']);
+  $cad_usuário->binParam(':email', $dados['email']);
+  $cad_usuário->binParam(':cpf', $dados['cpf']);
+  $cad_usuário->binParam(':telefone', $dados['telefone']);
+  $cad_usuario->execute();
+
+if ($cad_usuario->rowCount()){
+    $retorna = ['erro' => false, 'msg' => "<div class ='alert alert-success' role ='alert'> Usuário cadastrado com sucesso!</div>"];
+}else{
+   $retorna = ['erro' => true, 'msg' => "<div class ='alert alert-danger' role ='alert'>Erro: Usuário não cadastrado com sucesso!</div>"];
+}
+}
+
+echo json_encode($retorna);
